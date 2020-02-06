3696c9e961da8f0db1d802c8b6b3c489d4626f2c
robpalacios1 <robpalacios1@hotmail.com>
2020-02-06 13:46:38 -0500

    edit README.md file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -9,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-  printf("Last digit of\%d is greater than 5");
+printf("Last digit of \%d is greater than 5\n");
 }
 else if (n < 6)
 {
-printf("Last digit of\%d is less than 6 and not 0");
+printf("Last digit of \%d is less than 6 and not 0\n");
 }
 else
 {
-printf("Last digit of\%d is zero");
+printf("Last digit of \%d is zero\n");
 }
 return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -8,4 +8,5 @@
 7. Smile in the mirror
 8. Hexadecimal
 9. Patience, persistence and perspiration make an unbeatable combination for success 
-10. 00...99
+10. 00...99.
+
