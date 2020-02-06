1c7085a20bb54af2bbeeebc53ba1b302ce63a9e2
robpalacios1 <robpalacios1@hotmail.com>
2020-02-06 13:24:35 -0500

    fix code in emacs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+ * more headers goes there
+ *
+ * Return: Always 0 (Success)betty style doc for function main goes there
+ */
 int main(void)
 {
 int n;
