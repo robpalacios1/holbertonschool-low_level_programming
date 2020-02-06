ff8495e302a229c42ec08775904c9f4bf6c054ca
robpalacios1 <robpalacios1@hotmail.com>
2020-02-06 14:31:33 -0500

    fix code inside emacs

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,17 +11,20 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 5)
+printf("Last digit of ");
+printf("%d", n);
+printf(" is %d", n % 10);
+if ((n % 10) > 5)
 {
-printf("Last digit of \%d is greater than 5\n");
+printf("and is greater than 5\n");
 }
-else if (n < 6)
+else if ((n % 10) == 0)
 {
-printf("Last digit of \%d is less than 6 and not 0\n");
+printf("and is 0 \n");
 }
 else
 {
-printf("Last digit of \%d is zero\n");
+printf("and is less than 6 and not 0 \n");
 }
 return (0);
 }
