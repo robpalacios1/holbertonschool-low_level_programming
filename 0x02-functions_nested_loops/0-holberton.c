1ff19b6684930c878abacc8dc1f1306f0cecb59a
robpalacios1 <robpalacios1@hotmail.com>
2020-02-10 15:33:24 -0500

    add 0x02-functions_nested_loops dir and README.md file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char x[] = "Holberton";
+int i;
+for (i = 0; i < 9; i++)
+{
+putchar (x[i]);
+}
+putchar('\n');
+return (0);
+}
