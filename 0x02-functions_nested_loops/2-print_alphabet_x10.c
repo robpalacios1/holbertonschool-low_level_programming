514e7fc2180fd8d31c1640741586950add156073
robpalacios1 <robpalacios1@hotmail.com>
2020-02-11 10:26:18 -0500

    fix code in exercise number 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,26 +1,25 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School student
+ * print_alphabet_x10 - repeats the print 10 times.
  *
- * Return: returns 0 or void
+ * Return: nothing.
  */
 
 
 
 void print_alphabet_x10(void)
 {
-  int a;
-  int i;
+	int a;
+	int i;
 
-  for (i = 0; i < 10; i++)
-    {
-
-      for (a = 97; a <= 122; a++)
+	for (i = 0; i < 10; i++)
 	{
-	  _putchar(a);
+
+		for (a = 97; a <= 122; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
 	}
-      _putchar('\n');
-    }
-  return;
 }
