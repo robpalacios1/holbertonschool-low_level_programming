85955ee608ce62570dee5585eb0483753f37aa05
robpalacios1 <robpalacios1@hotmail.com>
2020-02-13 18:00:34 -0500

    fic code inside emacs

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,19 +8,14 @@ void print_square(int size)
 	int i;
 	int j;
 
-	if (size != 0)
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		for (j = 0; j < size; j++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar(35);
-			}
-			_putchar('\n');
+			_putchar(35);
 		}
-	}
-	else
-	{
 		_putchar('\n');
 	}
 }
