c3b93060bfdce0afccc0fa3c1d888ce3a369d9c5
robpalacios1 <robpalacios1@hotmail.com>
2020-02-13 17:24:24 -0500

    add 8-print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include "holberton.h"
+/**
+ *print_square - its print number.
+ *@size: parameter of size of function.
+ *Return: void.
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size != 0)
+	{
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
