fef7e9b385278c09f078a6af8d3fa80e4786d5e4
robpalacios1 <robpalacios1@hotmail.com>
2020-02-12 22:14:38 -0500

    add 104-fibonacci.c file

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+# define N 1000000000
+int main()
+{
+unsigned long a,b,r,i,a1,a2,b1,b2,r1,r2;
+a = 1;
+b = 2;
+r = a + b;
+printf ("%lu, %lu, ", a,b);
+for (i = 0; i < 86; i++)
+{
+printf("%lu, ", r);
+a = b;
+b = r;
+r = a + b;
+}
+b1 = b / N;
+b2 = b % N;
+r1 = r / N;
+r2 = r % N;
+for (i = 0; i < 9; i++)
+{
+printf("%lu %lu, ", r1, r2);
+a1 = b1;
+a2 = b2;
+b1 = r1;
+b2 = r2;
+r1 = a1 + b1 + (a2+b2)/N;
+r2 = (a2 +b2) % N;
+}
+printf ("%lu %lu\n", r1,r2);
+return 0;
+}
