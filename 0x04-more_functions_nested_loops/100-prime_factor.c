3b292ec6242cbf1c708c2e53d370ae5a03c61aec
robpalacios1 <robpalacios1@hotmail.com>
2020-02-14 17:51:35 -0500

    add 100-prime_factor.c file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - The prime factor;
+ *
+ *Return: always 0;
+ */
+int main(void)
+{
+	long n, i;
+
+	n = 612852475143;
+	for (i = 2; i < n; i++)
+	{
+		while (n % i == 0)
+			n = (n / i);
+	}
+	printf("%lu\n", n);
+	return (0);
+}
