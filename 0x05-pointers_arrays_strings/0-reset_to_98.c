467006ef9036692eb1035afeb27d352740cb5af9
robpalacios1 <robpalacios1@hotmail.com>
2020-02-17 14:17:22 -0500

    add 0x05-pointers_arrays_strings file and README.md

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+# include "holberton.h"
+# include <stdio.h>
+void reset_to_98(int *n);
+/**
+ * reset_to_98 - writes the character c to stdout
+ * @n: The character to print
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
