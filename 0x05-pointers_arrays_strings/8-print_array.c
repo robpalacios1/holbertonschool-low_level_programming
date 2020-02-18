73692c3b4f9e10ff39a034bc51a65a6f152eba92
robpalacios1 <robpalacios1@hotmail.com>
2020-02-18 13:32:27 -0500

    fix code inside emacs

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,17 +9,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n <= 0)
-		return;
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
-		{
-			printf("%d\n", a[i]);
-		}
+			printf("%d", a[i]);
 		else
-		{
 			printf("%d, ", a[i]);
-		}
 	}
+	printf("\n");
 }
