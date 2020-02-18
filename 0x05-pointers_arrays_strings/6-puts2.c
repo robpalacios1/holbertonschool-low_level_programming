8184ca915f79136b859a8fd30f5d8f99613071b4
robpalacios1 <robpalacios1@hotmail.com>
2020-02-17 22:44:41 -0500

    add 6-puts2.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * puts2 - length of one string
+ * @str: length of string.
+ * Return: return an integer called count.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
