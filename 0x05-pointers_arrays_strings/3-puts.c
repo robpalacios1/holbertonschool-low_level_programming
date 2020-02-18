ac1562d1fc7b65ff44bfa1975932315e63968acd
robpalacios1 <robpalacios1@hotmail.com>
2020-02-17 20:53:02 -0500

    add 3-main.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _puts - length of one string
+ * @str: string.
+ */
+void _puts(char *str)
+{
+	int string;
+
+	for(string = 0; str[string] != '\0'; string++)
+	{
+		_putchar(str[string]);
+	}
+	_putchar('\n');
+}
