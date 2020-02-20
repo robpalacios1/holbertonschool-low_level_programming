10e70af002a4e3318581b5bdea862752fc3a1204
robpalacios1 <robpalacios1@hotmail.com>
2020-02-20 14:07:47 -0500

    add 0x06-pointers_arrays_string dir and 0_strcat.c file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcat - string
+ * @dest: destiny
+ * @src: source
+ * Return: conc with two strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
