25434fb1cc791d10ed3244fd9afcfd8cc8b74009
robpalacios1 <robpalacios1@hotmail.com>
2020-02-27 22:40:17 -0500

    add 100-wildcmp.c and 100-main.c

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ *wildcmp - compare char with wilcard
+ *@s1: string 1
+ *@s2: string 2
+ *Return: return condition
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	}
+
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+
+}
