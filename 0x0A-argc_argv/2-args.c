a3396b026f38d53a3f6f0855baaa42233e900331
robpalacios1 <robpalacios1@hotmail.com>
2020-02-28 19:25:45 -0500

    add 2-args.c and args files

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main void.
+ *@argc:return value of a program.
+ *@argv:return size of array.
+ * Return: Always 0.
+ */
+int main( int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return(0);
+}
