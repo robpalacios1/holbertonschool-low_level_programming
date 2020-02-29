5af5acfa998f6c7520f46bcee67053f08b1dbb74
robpalacios1 <robpalacios1@hotmail.com>
2020-02-29 15:32:22 -0500

    add 3-mul.c file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
+
 /**
- * main - main void.
- *@argc:return value of a program.
- *@argv:return size of array.
- * Return: Always 0.
+ * is_num - checks if it is a digit
+ * @n: Number
+ * Return: If it is a number, retrun 1 else return 0
+ */
+int is_num(unsigned int n)
+{
+	return (n >= '0' && n <= '9');
+}
+
+/**
+ * _atoi - convert a string to a number
+ * @s: String to be converted
+ * Return: returns the number
+ */
+int _atoi(char *s)
+{
+	unsigned int num, i;
+	int sign;
+
+	sign = 1;
+	num = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (is_num(s[i]))
+
+		{
+			num = (s[i] - '0') + num * 10;
+		}
+		if (s[i + 1] == ' ')
+		{
+			break;
+		}
+		else if (s[i] == '-')
+		{
+			sign = sign * -1;
+		}
+	}
+	return (num * sign);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of string of arguments
+ *
+ * Return: 0, 1 for error
  */
 int main(int argc, char *argv[])
 {
@@ -11,19 +54,17 @@ int main(int argc, char *argv[])
 	int y;
 	int cont;
 
-	x = 0;
-	y = 0;
-	cont = 0;
-
-	if (argc == 3)
+	if (!(argc == 3))
 	{
-		x = *(*(argv + 1)) - 48;
-		y = *(*(argv + 2)) - 48;
-		cont = (x * y);
-		printf("%d\n", cont);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
 
-	printf("Error\n");
-	return (1);
+	x = _atoi(argv[1]);
+	y = _atoi(argv[2]);
+	cont = x * y;
+
+	printf("%d\n", cont);
+
+	return (0);
 }
