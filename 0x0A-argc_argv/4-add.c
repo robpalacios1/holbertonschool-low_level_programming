7958a18db3037654afc5088158fdefdd12458e27
robpalacios1 <robpalacios1@hotmail.com>
2020-03-01 11:41:15 -0500

    fix code inside emacs

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,86 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
- *main - print the multiplication of two numbers
- *@argc: is the numbers of counters.
- *@argv: is the array of arguments.
- *@s: pointer.
- *Return: return multiplication of a,b
+ * main - program adds positive numbers
+ * @argc: numbers of argument
+ * @argv: the arguments it receive
+ * Return: 0 Always
  */
 
-int _atoi(char *s);
-
-int adding(char **argv);
-
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	return (adding(argv + 1));
-}
+	int sum;
+	int val;
+	int i;
 
-int adding(char **s)
-{
-
-	int n;
-
-	n = _atoi(*s);
-
-	printf("%d\n", n);
-
-	if (!*s)
-	{
-		return (0);
-	}
-
-	if ((**s >= 32 && **s <= 47) || (**s > 57 && **s <= 126))
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	if (**s >= '0' && **s <= '9')
-	{
-		return (_atoi(*s) +  adding(s + 1));
-	}
-
-	return (0);
-
-}
-
-
-
-int _atoi(char *s)
-{
-	int n, contador;
-	int str, y, i;
-
-	str = 0;
-	y = 0;
+	sum = 0;
+	val = 0;
 	i = 0;
-	n = 1;
-	contador = 0;
 
-	for (i = 0 ; s[i] != '\0'; i++)
+	if (argc < 1)
 	{
-		if ((contador > 0) && (s[i] == ' '))
-
-		{
-			str = str;
-			break;
-		}
-
-		if (s[i] == '-')
-		{
-			n = n * -1;
-		}
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			contador++;
-			y = s[i] - '0';
-
-			str = (str * 10) + y;
-		}
+		printf("%d\n", 0);
 	}
 
-	return (str * n);
+	while (argc-- && argc > 0)
+	{
+		for (i = 0; argv[argc][i] != '\0'; i++)
+		{
+
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		val = atoi(argv[argc]);
+		sum += val;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
