df9b298e6d3fa7949b810c414418a09ee28e2d0a
robpalacios1 <robpalacios1@hotmail.com>
2020-03-03 13:30:54 -0500

    fix code inside emacs

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	s = (char *) malloc(sizeof(c) * size);
+	s = (char *) malloc(sizeof(c) * size + 1);
 
 	for (i = 0; i < size; i++)
 	{
