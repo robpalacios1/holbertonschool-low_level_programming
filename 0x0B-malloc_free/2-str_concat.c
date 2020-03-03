a18447e68623492c5e45be54a2b9ba4bfefedb68
robpalacios1 <robpalacios1@hotmail.com>
2020-03-03 16:17:55 -0500

    fix code inside emacs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,49 +11,33 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int l = 0;
-
+	int i = 0, j = 0, k = 0, l = 0;
 	char *s;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	while (s1[i])
-	{
 		i++;
-	}
 
 	while (s2[j])
-	{
 		j++;
-	}
 
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
 
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 
 	j = 0;
 
 	while (k < l)
 	{
 		if (k <= i)
-		{
 			s[k] = s1[k];
-		}
 
 		if (k >= i)
 		{
