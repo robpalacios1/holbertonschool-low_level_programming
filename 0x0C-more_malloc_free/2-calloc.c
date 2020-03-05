d02921a771f91f25486c182ce3e479808095eeea
robpalacios1 <robpalacios1@hotmail.com>
2020-03-05 18:21:02 -0500

    add 2-calloc.c and 2-main.c files

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *_calloc - allocates memory for an array
+ *@nmemb: number of elements in the array
+ *@size: size in bytes of the elements
+ * Return: Always 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+
+	if (p  == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
