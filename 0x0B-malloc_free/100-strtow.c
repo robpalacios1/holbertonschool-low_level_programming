e49cb105cf091d4abc0da6d689b1d1fb7137a036
robpalacios1 <robpalacios1@hotmail.com>
2020-03-05 14:22:07 -0500

    add 0-malloc_checked.c and 0-main.c files

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -16,10 +16,14 @@ int wrdcnt(char *s)
 		if (s[i] == ' ')
 		{
 			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+			{
 				n++;
+			}
 		}
 		else if (i == 0)
+		{
 			n++;
+		}
 	}
 	n++;
 	return (n);
@@ -37,40 +41,58 @@ char **strtow(char *str)
 	char **w;
 
 	if (str == NULL || *str == '\0')
+	{
 		return (NULL);
+	}
 	n = wrdcnt(str);
+
 	if (n == 1)
+	{
 		return (NULL);
+	}
 	w = (char **)malloc(n * sizeof(char *));
+
 	if (w == NULL)
+	{
 		return (NULL);
+	}
 	w[n - 1] = NULL;
 	i = 0;
+
 	while (str[i])
 	{
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
 			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
-				;
+			{
+			}
+
 			j++;
 			w[wc] = (char *)malloc(j * sizeof(char));
 			j--;
+
 			if (w[wc] == NULL)
 			{
 				for (k = 0; k < wc; k++)
+				{
 					free(w[k]);
+				}
 				free(w[n - 1]);
 				free(w);
 				return (NULL);
 			}
 			for (l = 0; l < j; l++)
+			{
 				w[wc][l] = str[i + l];
+			}
 			w[wc][l] = '\0';
 			wc++;
 			i += j;
 		}
 		else
+		{
 			i++;
+		}
 	}
 	return (w);
 }
