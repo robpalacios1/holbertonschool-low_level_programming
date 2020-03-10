0e8694536fe53d2156fc3439fb05ef9e512ccb6c
robpalacios1 <robpalacios1@hotmail.com>
2020-03-09 22:37:45 -0500

    add 3-main.c and add info dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 /**
- * struct dog - information about dogs
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * struct dog - date of dog
+ * @name: name member
+ * @age: age member
+ * @owner: owner member
+ *
+ * Description: Longer description
  */
 struct dog
 {
@@ -12,7 +14,10 @@ struct dog
 	float age;
 	char *owner;
 };
-#define dog_t struct dog
-void init_dog(struct dog *d, char *name, float *age, char *owner);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
