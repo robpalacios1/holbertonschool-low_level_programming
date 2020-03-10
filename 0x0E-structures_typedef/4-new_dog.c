39cc26828e0d39dcdd5aa158bc736f46b769b104
robpalacios1 <robpalacios1@hotmail.com>
2020-03-09 23:31:03 -0500

    fix code inside emacs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,26 +15,40 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	if (name == NULL || owner == NULL)
+	{
 		return (NULL);
+	}
 	dog = malloc(sizeof(dog_t));
+
 	if (dog == NULL)
+	{
 		return (NULL);
+	}
+
 	for (nl = 0; name[nl]; nl++)
-		;
+	{
+	}
 	nl++;
 	dog->name = malloc(nl * sizeof(char));
+
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
+
 	for (i = 0; i < nl; i++)
+	{
 		dog->name[i] = name[i];
+	}
 	dog->age = age;
+
 	for (ol = 0; owner[ol]; ol++)
-		;
+	{
+	}
 	ol++;
 	dog->owner = malloc(ol * sizeof(char));
+
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
@@ -42,6 +56,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (i = 0; i < ol; i++)
+	{
 		dog->owner[i] = owner[i];
+	}
 	return (dog);
 }
