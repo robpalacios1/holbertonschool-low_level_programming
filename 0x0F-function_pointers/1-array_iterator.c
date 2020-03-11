099d4310f9bef516f465ee542e4a6c6c27b9a79c
roberto palacios <robpalacios1@hotmail.com>
2020-03-11 16:04:14 -0500

    add 1-array_iterator.c and 1-main.c files

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ *array_iterator - iterator of array
+ *@array: array
+ *@size: size of the array.
+ *@action: is a pointer to the function you need to use
+ *
+ *Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
