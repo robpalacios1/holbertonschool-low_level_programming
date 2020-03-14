107c7f5dc0b55c7c30958384edc5a06f52b8e670
robpalacios1 <robpalacios1@hotmail.com>
2020-03-13 22:27:08 -0500

    add code inside 0-sum_them_all.c and create variadic_functions.h file

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ *sum_them_all - function that returns the sum of parameters.
+ *@n: numbers inside sum variable (parameters).
+ *
+ *Return: the sum of all parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int num;
+	unsigned int i;
+	int cont;
+	va_list parameters;
+
+	va_start(parameters, n);
+	num = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		cont = va_arg(parameters, int);
+		num = num + cont;
+	}
+	va_end(parameters);
+	return (num);
+}
