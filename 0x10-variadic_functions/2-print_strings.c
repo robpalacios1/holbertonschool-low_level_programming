416fcdbfe813477b03d4e5b669c86b4aeeea2d51
robpalacios1 <robpalacios1@hotmail.com>
2020-03-14 18:05:06 -0500

    fix code inside emacs

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,20 +20,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(valist, char*);
 
-		if (str)
-		{
-			printf("%s", str);
-		}
-		else
+		if (str == NULL)
 		{
 			printf("nil");
 		}
+		else
+		{
+			printf("%s", str);
+		}
 
-		if (i < n - 1 && separator)
+		if (i < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
 	}
-	printf("\n");
 	va_end(valist);
+	printf("\n");
 }
