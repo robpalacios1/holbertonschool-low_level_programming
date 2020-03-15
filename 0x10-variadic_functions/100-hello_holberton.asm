253a784c9e03d03665a4b1aaa6a8e47968583d19
robpalacios1 <robpalacios1@hotmail.com>
2020-03-15 13:02:03 -0500

    fix code inside emacs

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -6,21 +6,21 @@
 	;; nasm -f elf64 100-hello_holberton.asm && gcc 100-hello_holberton.o -o hello ./hello
 	;; -------------------------------------------------------------------------------------------
 
-	global     main
+	global main
 
-	section    .text
+	section .text
 
-main:
-	mov         rax, 1	; system call for writte
-	mov         rdi, 1	; file handle 1 is stdout
-	mov         rsi, msg 	; address of string to output
-	mov         rdx, msglen	 ; number of bytes
-	syscall			 ; invoke operating system to do the write
+	main:
+	mov rax, 1	; system call for writte
+	mov rdi, 1	; file handle 1 is stdout
+	mov rsi, msg 	; address of string to output
+	mov rdx, msglen	 ; number of bytes
+	syscall		; invoke operating system to do the write
 
-	mov         rax, 60	 ; system call for exit
-	xor         rdi, 0	 ; exit code 0
-	syscall			 ; invoke operating system to exit
+	mov rax, 60	 ; system call for exit
+	xor rdi, 0	 ; exit code 0
+	syscall		 ; invoke operating system to exit
 
-	section     .data
-msg:	db          "Hello, Holberton", 10 ; note the newline at the end
-msglen:	equ $ - msg
+	section .data
+	msg:	db "Hello, Holberton", 10 ; note the newline at the end
+	msglen:	equ $ - msg
