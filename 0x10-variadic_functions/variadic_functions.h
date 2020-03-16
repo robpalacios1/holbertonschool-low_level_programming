4ed289d28e39b833885a080095ec49473462ffaa
robpalacios1 <robpalacios1@hotmail.com>
2020-03-16 11:38:47 -0500

    betty style fixed

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,7 +11,7 @@ typedef struct wrap
 {
 	char *character;
 	void (*ptrfunc)();
-}wrapper;
+} wrapper;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
