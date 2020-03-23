765a23c861301370103b42defe6bc9ea6256c222
robpalacios1 <robpalacios1@hotmail.com>
2020-03-23 15:31:10 -0500

    add code inside emacs

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *print_listint - print all the elements.
+ *@h: head of list
+ *
+ *Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
