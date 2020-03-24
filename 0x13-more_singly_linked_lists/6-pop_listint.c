4e9fa5b8ec5840736d30eecf7e744b5ad3703713
Roberto Palacios <robpalacios1@hotmail.com>
2020-03-23 19:22:15 -0500

    add 6-pop_listint.c file

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *pop_listint - function that delete the head node 
+ *@head: head of the list.
+ *
+ *Return: void.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *a;
+	int i;
+
+	a = *head;
+
+	if (!a)
+	{
+		return (0);
+	}
+
+	i = a->n;
+	*head = a->next;
+	free(a);
+
+	return (i);
+}
