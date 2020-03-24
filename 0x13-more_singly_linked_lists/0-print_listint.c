2371aaf49092055b9d5b4cba3a57a111016088e8
Roberto Palacios <robpalacios1@hotmail.com>
2020-03-24 09:25:21 -0500

    fix code inside emacs

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,10 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i;
 
-	i = 0;
-
-	while (h != NULL)
+	for  (i = 0; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
 	}
 	return (i);
 }
---
+++ 0x13-more_singly_linked_lists/10-delete-nodeint.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *delete_nodeint_at_index - function that deletes the nodes.
+ *@head: head of the list.
+ *@index: is the index of the node.
+ *
+ *Return: address new node
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t i;
+}
