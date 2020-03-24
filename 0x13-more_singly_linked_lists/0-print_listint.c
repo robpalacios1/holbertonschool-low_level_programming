d0f141a67fd9fa41cf07b7745f356801ae60281a
Roberto Palacios <robpalacios1@hotmail.com>
2020-03-24 10:46:43 -0500

    fix code inside emacs

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i;
 
-	for  (i = 0; h != NULL; i++)
+	for (i = 0; h != NULL; i++)
 	{
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
 		h = h->next;
 	}
+
 	return (i);
 }
