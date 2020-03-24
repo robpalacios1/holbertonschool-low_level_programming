cdca7a7243e4aa95814aba1d74fba31b3bc94322
Roberto Palacios <robpalacios1@hotmail.com>
2020-03-24 14:54:41 -0500

    add code inside emacs

--- 0x13-more_singly_linked_lists/10-delete-nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete-nodeint.c
@@ -5,9 +5,23 @@
  *@head: head of the list.
  *@index: is the index of the node.
  *
- *Return: address new node
+ *Return: 1 if it suceeded, -1 if it dont.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t i;
+	int i;
+	listint_t *aux;
+
+	for (i = -1; *head != NULL; i++)
+	{
+		if (i + 1 == (int) index)
+		{
+			aux = (*head)->next;
+			free(*head);
+			*head = aux;
+			return (1);
+		}
+		head = &(*head)->next;
+	}
+	return (-1);
 }
