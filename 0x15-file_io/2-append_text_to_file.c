ee16d582bdd1bbf418f13171b7bb05b19bd6fdf5
Roberto Palacios <robpalacios1@hotmail.com>
2020-03-31 18:01:35 -0500

    add 2-append_text_to_file.c file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ *append_text_to_file - Functions that appends text at the end of a file
+ *
+ *@filename: filename.
+ *@text_content: text content.
+ *
+ *Return: count.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int a;
+	int b;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	a = open(filename, O_WRONLY | O_APPEND);
+
+	if (a == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (b = 0; text_content[b] != 0; b++)
+		{
+		}
+		write(a, text_content, b);
+	}
+	close(a);
+	return (-1);
+}
