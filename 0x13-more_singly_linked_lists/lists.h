0122cef26ee495e4ac0380c4c0179a664d8621f7
Roberto Palacios <robpalacios1@hotmail.com>
2020-03-31 13:48:15 -0500

    fix code inside emacs

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,6 +14,22 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct storage - singly linked list
+ * @address: adress
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
+typedef struct storage
+{
+	struct listint_s *address;
+	struct storage *next;
+} storage;
+
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
