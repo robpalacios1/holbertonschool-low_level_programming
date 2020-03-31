50d3371a683e1efc21d524b30da39cfcf304e2c5
Roberto Palacios <robpalacios1@hotmail.com>
2020-03-31 14:03:15 -0500

    add 0-main.c and fill info inside holberton.h file

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,16 @@
+/*Macro*/
+#ifndefine _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*Libraries I/O */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/*Prototypes*/
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
