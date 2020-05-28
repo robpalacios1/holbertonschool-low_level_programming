3ca7302705bae6aa902b291d5a6e049e471097a8
Roberto <robpalacios1@hotmail.com>
2020-05-28 11:48:38 -0500

    add 0-hash_table_create.c file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create - create a hash table
+ *@size: size of array
+ *Return: a new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash;
+	size_t i;
+
+	hash = malloc(sizeof(hash_table_t));
+	if (hash == NULL)
+	{
+		return (NULL);
+	}
+	hash->size = size;
+	hash->array = malloc(sizeof(hash_node_t) * size);
+	if (!hash->array)
+	{
+		free(hash);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		hash->array[i] = NULL;
+	}
+	return (hash);
+}
