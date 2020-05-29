f84536a7c48a0bdd810a8f16cc212b7554418f4a
Roberto <robpalacios1@hotmail.com>
2020-05-29 13:11:38 -0500

    fix code inside emacs

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,9 +10,8 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *ptr;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0)
-	{
 		return;
-	}
+
 	for (i = 0; i < ht->size; i++)
 	{
 		while (ht->array[i])
