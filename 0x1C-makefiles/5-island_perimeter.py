b56caa8053777f7a673f6ab68afd13cdb2f2a59f
Roberto <robpalacios1@hotmail.com>
2020-07-03 20:55:06 -0500

    fix code inside emacs

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,11 +9,11 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 if grid[i][j + 1] != 1:
-                    perimeter = perimeter + 1
+                    perimeter += 1
                 if grid[i][j - 1] != 1:
-                    perimeter = perimeter + 1
+                    perimeter += 1
                 if grid[i + 1][j] != 1:
-                    perimeter = perimeter + 1
+                    perimeter += 1
                 if grid[i - 1][j] != 1:
-                    perimeter = perimeter + 1
+                    perimeter += 1
     return perimeter
