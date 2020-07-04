b2ee473c88632cb5cb5deecd454095e049ec600e
Roberto <robpalacios1@hotmail.com>
2020-07-03 20:47:10 -0500

    add 5-island_perimeter.py file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''module perimeter'''
+
+
+def island_perimeter(grid):
+    '''function island_perimeter'''
+    perimeter = 0
+    for i in range(len(grid) - 1):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if grid[i][j + 1] != 1:
+                    perimeter = perimeter + 1
+                if grid[i][j - 1] != 1:
+                    perimeter = perimeter + 1
+                if grid[i + 1][j] != 1:
+                    perimeter = perimeter + 1
+                if grid[i - 1][j] != 1:
+                    perimeter = perimeter + 1
+    return perimeter
