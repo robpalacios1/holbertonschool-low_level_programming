07505ebb6746edc55e4eb940ca12042b41608527
Roberto <robpalacios1@hotmail.com>
2020-07-03 21:18:16 -0500

    fix task5 with equal to zero

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,15 +5,15 @@
 def island_perimeter(grid):
     '''function island_perimeter'''
     perimeter = 0
-    for i in range(len(grid) - 1):
-        for j in range(len(grid[i]) - 1):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i][j + 1] != 1:
+                if grid[i][j + 1] == 0:
                     perimeter = perimeter + 1
-                if grid[i][j - 1] != 1:
+                if grid[i][j - 1] == 0:
                     perimeter = perimeter + 1
-                if grid[i + 1][j] != 1:
+                if grid[i + 1][j] == 0:
                     perimeter = perimeter + 1
-                if grid[i - 1][j] != 1:
+                if grid[i - 1][j] == 0:
                     perimeter = perimeter + 1
     return perimeter
