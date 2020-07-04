94b674772c70a59ddf43adb680fd0cd481546f8f
Roberto <robpalacios1@hotmail.com>
2020-07-03 21:27:12 -0500

    fix condictionals statments into task5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,12 +8,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i][j + 1] == 0:
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter = perimeter + 1
-                if grid[i][j - 1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     perimeter = perimeter + 1
-                if grid[i + 1][j] == 0:
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter = perimeter + 1
-                if grid[i - 1][j] == 0:
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     perimeter = perimeter + 1
     return perimeter
